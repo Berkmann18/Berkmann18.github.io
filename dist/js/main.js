1e4b49d68532f05ad448a7d97f306b695e2accf8
Berkmann18 <maxieberkmann@gmail.com>
2018-08-02 01:51:16 +0100

    Menu Overlay + Desktop responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,3 +25,37 @@ const toggleMenu = () => {
 };
 
 menuBtn.addEventListener('click', toggleMenu);
+
+/* Fix the fucking portrait element that doesn't show up properly */
+const fixPortrait = (sz = '400px', url = 'url("./img/portrait.jpg")') => {
+  const portrait = document.querySelector('.menu-branding .portrait');
+  portrait.style.backgroundImage = url;
+  portrait.style.height = sz;
+  portrait.style.width = sz;
+  portrait.style.border = '3px solid #1aceee';
+  portrait.style.borderRadius = '50%';
+};
+
+const checkPortrait = (sz = '400px', img = 'url("./img/portrait.jpg")') => {
+  const portrait = document.querySelector('.menu-branding .portrait');
+  const cpt = getComputedStyle(portrait);
+  const lo = () => {
+    console.log(`Portrait:
+  bg: ${portrait.style.background}
+  height: ${portrait.style.height}
+  width: ${portrait.style.width}
+  border: ${portrait.style.border}
+  b-radius: ${portrait.style.borderRadius}`);
+    console.log(`Computed:
+  bg: ${cpt.background}
+  height: ${cpt.height}
+  width: ${cpt.width}
+  border: ${cpt.border}
+  b-radius: ${cpt.borderRadius}`);
+  };
+  if (cpt.height === '0px' || cpt.width === '0px' || cpt.backgroundImage === 'none') fixPortrait(sz, img);
+  // console.log('After');
+  lo();
+};
+
+fixPortrait();
